f4a79e31d83b5fe6a96ce644297da654b222f254
Mick Sexton and Mara Timberlake <mara@>
2017-07-05 14:10:52 -0700

    add fix for UI

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -18,7 +18,12 @@ public class App {
 
     get("/result", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
+      ChangeMachine myChangeMachine = new ChangeMachine();
+      Float flCash = Float.valueOf(request.queryParams("cash"));
       model.put("cash", request.queryParams("cash"));
+      model.put("flCash", flCash);
+      model.put("template", "templates/result.vtl");
+      model.put("finalResult", myChangeMachine.makeChange(flCash));
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
   }
